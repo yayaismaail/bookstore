e4d76647f674ccd583adf103d41ace8ec700da11
ismail halliru muhammad <ismailhm888@gmail.com>
2023-09-30 05:57:32 +0100

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ const Form = () => (
     <form>
       <input type="text" placeholder="Book Title" />
       <input type="text" placeholder="Author Name" />
+      <button type="submit" className="add-btn">Add</button>
     </form>
   </>
 );
