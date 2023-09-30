09104bf76dca67cf51705550534e494349a288e6
ismail halliru muhammad <ismailhm888@gmail.com>
2023-09-30 10:11:25 +0100

    Update Books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Form from './Form';
+import BookList from './BookList';
 
-const Books = ({ title, author }) => (
-  <>
-    <div>
-      <h2 className="book">{title}</h2>
-      <p className="by">by</p>
-      <p className="author">{author}</p>
-      <button type="button" className="remove-btn">Delete</button>
-    </div>
-    <Form />
-  </>
-);
-
-Books.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+const Books = () => {
+  const books = [
+    { title: '1000 Spatans', author: 'Romans', id: 1 },
+    { title: '2000 Spatans', author: 'Romans', id: 2 },
+    { title: '3000 Spatans', author: 'Romans', id: 3 },
+  ];
+  return (
+    <>
+      <h1 className="title">Book Lists</h1>
+      <div className="books">
+        {books.map((book) => (
+          <ul className="booklists" key={book.title}>
+            <li className="lists">{book.title}</li>
+            <li className="lists">{book.author}</li>
+            <button type="button" className="remove-btn">Delete</button>
+          </ul>
+        ))}
+      </div>
+      <div className="book-form">
+        <BookList />
+      </div>
+    </>
+  );
 };
 
 export default Books;
