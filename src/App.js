d829456e41d67742cc98bb0bad061220a059ff88
ismail halliru muhammad <ismailhm888@gmail.com>
2023-10-03 14:31:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className="entry-point">
       <Navbar />
       <Routes>
         <Route path="/" element={<BookList />} />
